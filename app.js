ad9f28a34726de5e6e89887b5f7c7f7a72767aff
BrandonMarczak <brandonmarczak@yahoo.co.uk>
2018-04-23 14:05:03 +0000

    slight updates

--- app.js
+++ app.js
@@ -7,36 +7,46 @@ var app     = express();
 
 app.get('/', function(req, res){
   
+  //The page that we will scrape
   var  url = 'https://caseblocks.com';
-  console.log("URL: " + url);
-
+  console.log('URL: ' + url);
+  
+   // The request call structure with a call back function
   request(url, function(error, response, body){
     if(!error && response.statusCode == 200){
-      
-      console.log("Status code: " + response.statusCode);
-      
+      console.log('Status code: ' + response.statusCode);
+      // Applying the cheerio library for jQuery functionality. With this library we will start and pick our specific elements
       var $ = cheerio.load(body);
+       // Defining the specific words that we want to capture
       var url, title, statusCode;
+      // Once captured, we will store them in the json file
       var json = { url : "", title : "", statusCode : ""};
-      
-      // Scraping the title
+      // First, scraping the title
       $('title').filter(function(){
         title = $('title').text();
-        
-        console.log("Page title:  " + title);
-
+        console.log('Page title:  ' + title);
+        console.log();
+        //Storing our Title in the json object.
         json.title = title;
         
+        // Struggled to find an approach to fetch and store the URL and status code in the json and csv files.
+        
       });
-      
     }
     
     // Save search results as an json
     fs.writeFile('result.json', JSON.stringify(json, null, 4), function(err){
+      console.log('File successfully stored! - Check your directory for result.json');
       console.log();
-      console.log('File successfully stored! - Check your project directory for the result.json file');
-      console.log();
-      
+    });
+    
+    //Save file as a CSV 
+    fs.writeFile('result.csv', title, 'utf8', function (err) {
+      if(err) {
+        console.log('Error occured');
+      } else {
+        console.log('File successfully stored! - Check your directory for result.csv');
+      }
     });
 
     res.send('Look at the console!');
@@ -44,9 +54,16 @@ app.get('/', function(req, res){
   });
 });
 
+//Listens for the server
 var server = app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server has started at port: " + server.address().port);
+    console.log('Server has started at port: ' + server.address().port);
     console.log();
 });
+
+// ** Apply the code below for local server **
+
+// app.listen(3000, function () {
+//   console.log('Listening on port 3000!');
+// });
 
 exports = module.exports = app;
